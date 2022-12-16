bcb79fa2c68fb5453f22c529eef12881c5d6c06b
gotjd709 <gotjd709@gmail.com>
2022-12-16 10:32:05 +0900

    revision

--- datagen.py
+++ datagen.py
@@ -70,12 +70,12 @@ class TensorData(Dataset):
     def __getitem__(self, index):
         batch_x = np.zeros((2,) + self.image_size + (3,), dtype='float32') 
         x_data = batch_x.copy()
-        batch_y = np.zeros(self.image_size + (6,), dtype='uint8')
+        batch_y = np.zeros(self.image_size + (self.classes,), dtype='uint8')
         img_path1, img_path2, mask_path = self.path_list[index]
         batch_x[0] = cv2.imread(img_path1)/255
         batch_x[1] = cv2.imread(img_path2)/255
         mask = cv2.imread(mask_path, 0)
-        for i in range(6):
+        for i in range(self.classes):
             batch_y[...,i] = np.where(mask==i, 1, 0)
         sample = self.augmentation(image=batch_x[0], image1=batch_x[1], mask=batch_y) 
         x_data[0], x_data[1], y_data = sample['image'], sample['image1'], sample['mask']
